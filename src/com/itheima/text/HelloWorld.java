808b0431d16b3427b2896ade0736dbfca206aa7c
Loading8210 <mcjzz1@outlook.com>
2026-04-25 22:14:22 +0800

    第一个程序

---
+++ src/com/itheima/text/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.itheima.text;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //输出一段话，内容为：你好我是JAVA
+        System.out.println("你好我是JAVA");
+    }
+}
+//这是一行注释
+//这是一行注释
