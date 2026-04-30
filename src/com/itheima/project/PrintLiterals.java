1bdce16bf877509ba46e6dbe4db4baf9c27116e2
Loading8210 <mcjzz1@outlook.com>
2026-04-30 22:02:04 +0800

    update:字面量，输出语句，Notes

---
+++ src/com/itheima/project/PrintLiterals.java
@@ -0,0 +1,14 @@
+package com.itheima.project;
+
+import java.lang.reflect.Type;
+
+public class PrintLiterals {
+    public static void main(String[] args) {
+        //打印地球信息：平均半径：6371 km,表面积：5.1*10^8 km^2,赤道周长：4*10^4 km
+        System.out.println("名称：地球");
+        System.out.println(6371);
+        System.out.println(5.1);
+        System.out.println('4');
+
+    }
+}
