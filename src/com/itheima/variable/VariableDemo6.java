779d5e5111b153feadf284179faeb04d48435ddd
Loading8210 <mcjzz1@outlook.com>
2026-05-03 18:37:04 +0800

    2026-05-03:键盘录入练习

---
+++ src/com/itheima/variable/VariableDemo6.java
@@ -0,0 +1,16 @@
+package com.itheima.variable;
+
+import java.util.Scanner;
+
+public class VariableDemo6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入第一个数：");
+        double a = sc.nextDouble();
+        System.out.println("请输入第二个数：");
+        double b = sc.nextDouble();
+        double c = a + b;
+        System.out.println("两个数的和是：" + c);
+    }
+
+}
