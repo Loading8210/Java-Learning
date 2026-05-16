f715a64a50fa0b42e52433579f0ccccf0d661f01
Loading8210 <mcjzz1@outlook.com>
2026-05-16 13:11:06 +0800

    2026-05-16:字符计算

---
+++ src/com/itheima/operator/OperatorDemo5.java
@@ -0,0 +1,11 @@
+package com.itheima.operator;
+
+public class OperatorDemo5 {
+    public static void main(String[] args) {
+        char c = '/';
+        int result1 = c;
+        System.out.println(c);
+        System.out.println(result1);
+    }
+
+}
